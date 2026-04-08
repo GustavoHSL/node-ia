300c8efb0a0af9887bfe4ef4dec5fbbfd2c6c69d
Gustavo Lopes <gustavolop@gmail.com>
2026-04-08 09:41:50 -0300

    feat: setup inicial com OpenAI API

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,20 @@
 import dotenv from 'dotenv'
+import OpenAI from 'openai';
 dotenv.config();
 
-console.log('API KEY', process.env.OPEN_AI_API_KEY);
+async function main() {
+
+const client = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY,
+})    
+
+const response = await client.responses.create({
+    model:"gpt-4o-mini",
+    input: "Escreva uma curta historia de dormir sobre unicornio",
+    max_output_tokens: 80
+});
+
+console.log(response.output[0].content[0].text);
+}
+
+main()
