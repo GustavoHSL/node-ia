66bab148abfcdae5fdc0e2455b635705d87a2186
Gustavo Lopes <gustavolop@gmail.com>
2026-04-08 10:25:02 -0300

    feat: explorando roles diferentes de mensagens e aprimorando codigo

--- src/index.ts
+++ src/index.ts
@@ -2,19 +2,37 @@ import dotenv from 'dotenv'
 import OpenAI from 'openai';
 dotenv.config();
 
-async function main() {
-
 const client = new OpenAI({
     apiKey: process.env.OPENAI_API_KEY,
 })    
 
-const response = await client.responses.create({
-    model:"gpt-4o-mini",
-    input: "Escreva uma curta historia de dormir sobre unicornio",
-    max_output_tokens: 80
-});
 
-console.log(response.output[0].content[0].text);
+async function generateText() {
+    const completion = await client.chat.completions.create({
+        model:"gpt-4o-mini",
+        max_completion_tokens: 80,
+        //temperature: 0.2, //balanço entre determinismo x criativo de 0(determinista) a 2(criativo), 
+        messages: [
+            {
+                role: "developer",
+                content: "Use emojis a cada 2 palavras. Gere um texto com no máximo uma frase."
+            },
+            {
+                role: "user", //developer, assistant
+                content: "Escreva uma mensagem de uma frase sobre hobbits",
+            },
+            {
+                role: "assistant",
+                content: "Os 🧙‍♂️ hobbits 🥔 são 🌿 criaturas 🏡 adoráveis que 🌈 amam a 🍵 simplicidade e 🌍 aventuras inesperadas!",
+            },
+            {
+                role: "user",
+                content: "Obrigado",
+            }
+        ],
+    });
+
+    console.log(completion.choices[0].message.content);
 }
 
-main()
+generateText();
